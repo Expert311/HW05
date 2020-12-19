849ee3c4cc94c6642bbef2a3f2d58184b4560ff5
Expert311 <rublik.magazin@gmail.com>
2020-12-19 06:22:19 +0300

    HW05

--- src/academy/belhard/Main.java
+++ src/academy/belhard/Main.java
@@ -1,8 +1,13 @@
 package academy.belhard;
 
+import academy.belhard.entity.User;
+
 public class Main {
 
     public static void main(String[] args) {
+        User u1 = new User("Tom", "Hardy", "tom.hardy@mail.ru", "1234");
 
+        System.out.println(u1.getFullInfo());
+        System.out.println(u1.isPasswordCorrect("7896"));
     }
 }
--- src/academy/belhard/entity/User.java
+++ src/academy/belhard/entity/User.java
@@ -41,14 +41,4 @@ public class User extends Person {
     }
 
 
-    public static void main(String[] args) {
-
-        User u1 = new User("Tom", "Hardy", "tom.hardy@mail.ru", "1234");
-
-        System.out.println(u1.getFullInfo());
-        System.out.println(u1.isPasswordCorrect("7896"));
-
-    }
-
-
 }
